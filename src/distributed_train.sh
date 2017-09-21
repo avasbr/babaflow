ca32ff64c628c1b819c3cfc8d2b44b2c90504b99
babasarala <babasarala@Donalds-iMac.local>
2017-09-21 15:32:00 -0700

    Add script

--- src/distributed_train.sh
+++ src/distributed_train.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES= python dumbnet_distributed_training.py --job_name="ps" --task_index=0
-CUDA_VISIBLE_DEVICES=0 python dumbnet_distributed_training.py --job_name="worker" --task_index=0
-CUDA_VISIBLE_DEVICES=1 python dumbnet_distributed_training.py --job_name="worker" --task_index=1
+MODEL_DIRPATH=$1
+
+CUDA_VISIBLE_DEVICES= python dumbnet_distributed_training.py \
+--model_dirpath $MODEL_DIRPATH \
+--job_name "ps" --task_index 0
+
+CUDA_VISIBLE_DEVICES=0 python dumbnet_distributed_training.py \
+--job_name "worker" --task_index 0 \
+--model_dirpath $MODEL_DIRPATH
+
+CUDA_VISIBLE_DEVICES=1 python dumbnet_distributed_training.py \
+--job_name "worker" --task_index 1 \
+--model_dirpath $MODEL_DIRPATH
